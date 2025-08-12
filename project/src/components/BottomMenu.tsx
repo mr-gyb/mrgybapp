2ef7766f5ffad262811215639d838d8b93908537
kevinlee1989 <kjmin99999@gmail.com>
2025-08-12 04:31:41 -0700

    integrate with Nanditha's work

--- project/src/components/BottomMenu.tsx
+++ project/src/components/BottomMenu.tsx
@@ -24,7 +24,7 @@ const BottomMenu: React.FC = () => {
 
   const menuItems = [
     {
-      path: '/home',
+      path: '/homepage',
       icon: Home,
       label: 'Home',
       isCustomNav: false
